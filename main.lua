297a9aafb49b3c878cef8c9865bf293d0ab21ecc
kesava-wow <kesava-wow@users.noreply.github.com>
2017-11-16 14:05:31 +0000

    whitespace, comments, bump toc

--- main.lua
+++ main.lua
@@ -1,38 +1,38 @@
---[[
-	EasyDeleteConfirm
-	Kesava-Auchindoun
-	All rights reserved.
-]]
+--------------------------------------------------------------------------------
+-- EasyDeleteConfirm
+-- By Kesava at curse.com
+-- All rights reserved
+--------------------------------------------------------------------------------
 local folder, ns = ...
 local addon = CreateFrame('Frame','EasyDeleteConfirmFrame')
 
 function addon:DELETE_ITEM_CONFIRM(...)
-	if StaticPopup1EditBox:IsShown() then
-		StaticPopup1EditBox:Hide()
-		StaticPopup1Button1:Enable()
+    if StaticPopup1EditBox:IsShown() then
+        StaticPopup1EditBox:Hide()
+        StaticPopup1Button1:Enable()
 
-		local link = select(3,GetCursorInfo())
+        local link = select(3,GetCursorInfo())
 
-		addon.link:SetText(link)
-		addon.link:Show()
-	end
+        addon.link:SetText(link)
+        addon.link:Show()
+    end
 end
 
 function addon:ADDON_LOADED(loaded_addon)
-	if loaded_addon ~= folder then return end
+    if loaded_addon ~= folder then return end
 
-	-- create item link container
-	addon.link = StaticPopup1:CreateFontString(nil, 'ARTWORK', 'GameFontHighlight')
-	addon.link:SetPoint('CENTER', StaticPopup1EditBox)
-	addon.link:Hide()
+    -- create item link container
+    addon.link = StaticPopup1:CreateFontString(nil, 'ARTWORK', 'GameFontHighlight')
+    addon.link:SetPoint('CENTER', StaticPopup1EditBox)
+    addon.link:Hide()
 
-	StaticPopup1:HookScript('OnHide', function(self)
-		addon.link:Hide()
-	end)
+    StaticPopup1:HookScript('OnHide', function(self)
+        addon.link:Hide()
+    end)
 end
 
 addon:SetScript('OnEvent', function(self,event,...)
-	self[event](self,...)
+    self[event](self,...)
 end)
 
 addon:RegisterEvent('ADDON_LOADED')
